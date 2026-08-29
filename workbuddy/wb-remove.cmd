dd418659fcfbfa6cf02fa8f18eb64ceb142803bd
Alexis-7ero <254376389+Alexis-7ero@users.noreply.github.com>
2026-08-29 20:13:13 +0800

    revert WorkBuddy integration (target app reinstalled fresh; analysis code preserved locally as workbuddy-wip)

--- workbuddy/wb-remove.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-rem WorkBuddy deep-integration uninstaller
-chcp 65001 >nul
-cd /d "%~dp0"
-node wb-setup.mjs remove
-pause
--- workbuddy/wb-setup.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-rem WorkBuddy deep-integration installer (env + MCP tools + global memory)
-chcp 65001 >nul
-cd /d "%~dp0"
-node wb-setup.mjs install
-pause
